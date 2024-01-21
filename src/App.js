81c20746f0713184f5c09bcfc811fd4b194f41d1
Dmitriy Protasov <Protasov.Dmitriy@gmail.com>
2024-01-21 20:58:25 +0300

    Test commit message!

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
             <div className="d-flex justify-between align-center">
               <div className="d-flex flex-column">
                 <span>Цена:</span>
-                <b>12 999 руб.</b>
+                <b>12999 руб.</b>
               </div>
               <button className="button">
                 <img src="/img/add_cart.svg" alt="Add" width={11} height={11} />
