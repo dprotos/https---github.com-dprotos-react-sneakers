4ee8731870c905bab3cc4eea112f33cc9442ca8c
Dmitriy Protasov <Protasov.Dmitriy@gmail.com>
2024-01-25 11:01:58 +0300

    Урок 3 1. Массив компонентов. маппинг пропсов 2. модульный scss

--- src/App.js
+++ src/App.js
@@ -2,6 +2,33 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
+const arr = [
+  {
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 13000,
+    imageUrl: "/img/sneakers/1.jpg",
+    key:1,
+  },
+  {
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 14000,
+    imageUrl: "/img/sneakers/2.jpg",
+    key:2,
+  },
+  {
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 13000,
+    imageUrl: "/img/sneakers/3.jpg",
+    key:3,
+  },
+  {
+    title: "Кроссовки Puma X Aka Boku Future Rider",
+    price: 9000,
+    imageUrl: "/img/sneakers/4.jpg",
+    key:4,
+  },
+];
+
 function App() {
   const cardIsOpen = "none";
   return (
@@ -22,10 +49,9 @@ function App() {
         </div>
 
         <div className="items d-flex">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {arr.map((obj) => (
+            <Card title={obj.title} price={obj.price} imageUrl={obj.imageUrl} key={obj.key} onClick={() => alert(obj.title)} />
+          ))}
         </div>
       </div>
     </div>
